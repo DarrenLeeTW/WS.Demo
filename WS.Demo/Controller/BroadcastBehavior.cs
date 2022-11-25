883a577b7f43b62850543bb7b8febfa372e7f587
darren.li <jinewen@gmail.com>
2022-11-25 21:48:55 +0800

    加入專案檔案。

---
+++ WS.Demo/Controller/BroadcastBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading;
+using WebSocketSharp.Server;
+
+namespace WS.Demo.Controller {
+    public class BroadcastBehavior : WebSocketBehavior {
+        public BroadcastBehavior() { }
+        protected override void OnOpen() {
+
+            while (true) {
+                string msg = null;
+                MessageQueueSingleton.Instance().GetMsg(out msg);
+                if (msg != null) {
+                    Sessions.BroadcastAsync(Encoding.UTF8.GetBytes(msg), null);
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
---
+++ WS.Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using WS.Demo.Controller;
+using WebSocketSharp.Server;
+
+
+namespace WS.Demo {
+    class Program {
+        static void Main(string[] args) {
+            var msgCreater = new Thread(() => {
+                while (true) {
+                    MessageQueueSingleton.Instance().AddMsg();
+                    Thread.Sleep(250);
+                }
+               });
+            msgCreater.Start();
+
+            var wssv = new WebSocketServer(55688);
+            wssv.AddWebSocketService<BroadcastBehavior>("/Broadcast");
+            wssv.Start();
+            if (wssv.IsListening) {
+                Console.WriteLine("Listening on port {0}, and providing WebSocket services:", wssv.Port);
+                foreach (var path in wssv.WebSocketServices.Paths) {
+                    Console.WriteLine("- {0}", path);
+                }
+            }
+
+            Console.WriteLine("\nPress Enter key to stop the server...");
+            Console.ReadLine();
+            MessageQueueSingleton.Instance().FreeQueue();
+            msgCreater.Abort();
+            wssv.Stop();
+
+        }
+    }
+}
